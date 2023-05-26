e5a6e2edc2c8e13ac4b05b674ff20b662c53281c
skygroundwater <o.metelev2020@yandex.ru>
2023-05-26 08:52:30 +0300

    master commit

---
+++ src/main/java/com/telegrambotanimalshelter/TgBotAnimalShelterApplication.java
@@ -0,0 +1,13 @@
+package com.telegrambotanimalshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgBotAnimalShelterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TgBotAnimalShelterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/telegrambotanimalshelter/TgBotAnimalShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telegrambotanimalshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgBotAnimalShelterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
