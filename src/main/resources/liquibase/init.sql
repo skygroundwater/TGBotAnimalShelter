374d0226145001ff0bdff4522ee12f613b18fa4d
Nitro <o.metelev2020@yandex.ru>
2023-06-02 14:55:45 +0300

    Part 1.
    Реализовал кнопки и алгоритм прохода по запросам. Думаю как добавить функционал принятия строчных данных от пользователя бота.

--- src/main/resources/liquibase/init.sql
+++ src/main/resources/liquibase/init.sql
@@ -2,4 +2,55 @@
 
 -- changeset oleg:1
 
+create table petowners
+(
+    petowner_id   bigint    not null
+        constraint petowners_pk
+            primary key,
+    first_name    varchar   not null,
+    last_name     varchar   not null,
+    username      varchar   not null,
+    registered_at timestamp not null,
+    has_pets      boolean
+);
+
+alter table petowners
+    owner to postgres;
+
+create table cats
+(
+    nickname              varchar   not null,
+    is_chipped            boolean   not null,
+    registered_at         timestamp not null,
+    shelter               varchar   not null,
+    pet_owner_petowner_id bigint    not null
+        constraint petowner_id_fk_cats
+            references petowners,
+    id                    bigserial
+        constraint cats_pk
+            primary key
+);
+
+alter table cats
+    owner to postgres;
+
+create table dogs
+(
+    nickname              varchar   not null,
+    is_chipped            boolean   not null,
+    registered_at         timestamp not null,
+    shelter               varchar   not null,
+    pet_owner_petowner_id bigint    not null
+        constraint petowner_id_fk
+            references petowners,
+    id                    bigserial
+        constraint dogs_pk
+            primary key
+);
+
+alter table dogs
+    owner to postgres;
+
+
+
 
