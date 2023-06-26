3ec7da110ef852fcf085c2ff2e45682fc518f553
Ryabinin85 <Ryabinin85@spartak.ru>
2023-06-26 20:33:25 +0300

    add DTO for Cats and Dogs to controllers

--- src/main/java/com/telegrambotanimalshelter/controllers/CatController.java
+++ src/main/java/com/telegrambotanimalshelter/controllers/CatController.java
@@ -1,25 +1,31 @@
 package com.telegrambotanimalshelter.controllers;
 
+import com.telegrambotanimalshelter.dto.animals.CatDTO;
 import com.telegrambotanimalshelter.models.animals.Cat;
 import com.telegrambotanimalshelter.services.petservice.PetService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import static com.telegrambotanimalshelter.services.mapper.CatMapper.convertToCat;
 
 @RestController
 @RequestMapping("/cats")
 public class CatController {
 
     private final PetService<Cat> catsService;
+    private final ModelMapper modelMapper;
 
-    public CatController(@Qualifier("catsServiceImpl") PetService<Cat> catsService) {
+    public CatController(@Qualifier("catsServiceImpl") PetService<Cat> catsService, ModelMapper modelMapper) {
         this.catsService = catsService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping
-    public ResponseEntity<Cat> postCat(@RequestBody Cat cat) {
-        return ResponseEntity.ok(catsService.postPet(cat));
+    public ResponseEntity<Cat> postCat(@RequestBody CatDTO catDTO) {
+        return ResponseEntity.ok(catsService.postPet(convertToCat(catDTO, modelMapper)));
     }
 
     @GetMapping("/{id}")
@@ -28,12 +34,12 @@ public class CatController {
     }
 
     @DeleteMapping
-    public ResponseEntity<HttpStatus> deleteCat(@RequestBody Cat cat) {
-        return ResponseEntity.ok(catsService.deletePet(cat));
+    public ResponseEntity<HttpStatus> deleteCat(@RequestBody CatDTO catDTO) {
+        return ResponseEntity.ok(catsService.deletePet(convertToCat(catDTO, modelMapper)));
     }
 
     @PutMapping("/put")
-    public ResponseEntity<Cat> putCat(@RequestBody Cat cat) {
-        return ResponseEntity.ok(catsService.putPet(cat));
+    public ResponseEntity<Cat> putCat(@RequestBody CatDTO catDTO) {
+        return ResponseEntity.ok(catsService.putPet(convertToCat(catDTO, modelMapper)));
     }
 }
