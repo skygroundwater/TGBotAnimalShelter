8696176f78f5ee28f7f9a5c9a9be24903f104820
Nitro <o.metelev2020@yandex.ru>
2023-07-04 17:52:33 +0300

    Начинаем тестирование

--- src/test/java/com/telegrambotanimalshelter/TgBotAnimalShelterApplicationTests.java
+++ src/test/java/com/telegrambotanimalshelter/TgBotAnimalShelterApplicationTests.java
@@ -1,10 +1,15 @@
 package com.telegrambotanimalshelter;
 
+import com.pengrad.telegrambot.TelegramBot;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TgBotAnimalShelterApplicationTests {
+
+
 
     @Test
     void contextLoads() {
